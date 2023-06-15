2794cbd0a77248f36927ee8fa09e5c5b4bf9f58f
Caio Soares <caiofasoares@gmail.com>
2023-06-14 22:14:41 -0300

    Initial Commit

---
+++ Trabalho_BancoDeDados/Trabalho_BancoDeDadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trabalho_BancoDeDadosApp.swift
+//  Trabalho_BancoDeDados
+//
+//  Created by Caio Soares on 14/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Trabalho_BancoDeDadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
